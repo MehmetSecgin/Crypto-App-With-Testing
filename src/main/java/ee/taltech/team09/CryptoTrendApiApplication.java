e335f725d89e019d00c43a22cd83f6f307b8e39b
mesecg <mesecg@ttu.ee>
2021-10-09 21:13:18 +0300

    Initial commit

---
+++ src/main/java/ee/taltech/team09/CryptoTrendApiApplication.java
@@ -0,0 +1,13 @@
+package ee.taltech.team09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoTrendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoTrendApiApplication.class, args);
+	}
+
+}
